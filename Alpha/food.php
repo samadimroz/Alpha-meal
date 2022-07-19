bed02016314b91e0771a7a03969fe2111137d439
samadimroz <71924557+samadimroz@users.noreply.github.com>
2022-07-19 22:18:04 +0530

    Add files via upload

---
+++ Alpha/food.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Alpha foodz</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="vendors/css/normalize.css">
+    <link rel="stylesheet" href="vendors/css/grid.css">
+
+     
+
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;1,300&display=swap"
+        rel="stylesheet">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="resources/css/style.css">
+    <link rel="stylesheet" href="resources/css/queries.css">
+
+ 
+            <!-- WhatsApp cdn -->
+
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+     <!-- //WhatsApp cdn -->
+  
+
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="row">
+                <img class="logo" src="https://www.pinclipart.com/picdir/big/547-5478238_cook-clipart-caterer-catering-food-logo-png-transparent.png" alt="logo-img">
+                <ul class="main-nav">
+                    <li><a href="#feature">Food delivery</a></li>
+                    <li><a href="menucart.php">Menu</a></li>
+                    <!--<li><a href="#city">Our cities</a></li> -->
+                    <li><a href="#signup">Sign up</a></li>
+                </ul>
+            </div>
+
+        </nav>
+        <div class="hero-text-box">
+            <h1>A healthy meal delivered to your door, every single day </h1>
+            <a class="btn btn-full" href="#plans">I'm Hungry</a>
+            <a class="btn btn-ghost" href="menucart.php">Show me more</a>
+
+        </div>
+    </header>
+    <section class="section-features" id="feature">
+        <div class="row">
+            <h2>Get food fast &mdash; Not fast food</h2>
+            <p class="long-copy">Hello, we're Alpha Meal, your new premium food delivery service. We know you're always
+                busy. No time for cooking. So let us take care of that, we're really good at it, we promise!
+
+            </p>
+
+        </div>
+        <div class="row">
+            <div class="col span_1_of_4 box">
+                <ion-icon class="icon-big" name="infinite"></ion-icon>
+                <h3>Up to 365 days/year</h3>
+                <p>Never cook again! We really mean that. Our subscription plans include up to 365 days/year coverage.
+                    You can also choose to order more flexibly if that's your style.</p>
+            </div>
+            <div class="col span_1_of_4 box">
+                <ion-icon class="icon-big" name="alarm"></ion-icon>
+                <h3>Ready in 20 minutes</h3>
+                <p>You're only twenty minutes away from your delicious and super healthy meals delivered right to your
+                    home. We work with the best chefs in each town to ensure that you're 100% happy.</p>
+            </div>
+            <div class="col span_1_of_4 box">
+                <ion-icon class="icon-big" name="ice-cream"></ion-icon>
+                <h3>100% organic</h3>
+                <p>All our vegetables are fresh, organic and local. Animals are raised without added hormones or
+                    antibiotics. Good for your health, the environment, and it also tastes better!</p>
+            </div>
+            <div class="col span_1_of_4 box">
+                <ion-icon class="icon-big" name="cart"></ion-icon>
+                <h3>Order anything</h3>
+                <p>We don't limit your creativity, which means you can order whatever you feel like. You can also choose
+                    from our menu containing over 100 delicious meals. It's up to you!</p>
+            </div>
+
+        </div>
+
+       <!-- WhatsApp -->
+     <a href="https://wa.me/917970695884?text=Hey!" class="whatsapp_float" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
+          <!-- // WhatsApp -->
+
+
+    </section>
+    <section class="section-meals" data-aos="fade-in" data-aos-duration="2000">
+        <ul class="meals-showcase">
+            <li>
+                <figure class="meal-photo">
+                    <img src="resources/img/A.jpg" alt="food-image">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="resources/img/B.jpg" alt="food-image">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="resources/img/C.jpg" alt="food-image">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="resources/img/D.jpg" alt="food-image">
+                </figure>
+            </li>
+
+        </ul>
+        <ul class="meals-showcase">
+            <li>
+                <figure class="meal-photo">
+                    <img src="resources/img/E.jpg" alt="food-image">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="resources/img/F.jpg" alt="food-image">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="resources/img/G.jpg" alt="food-image">
+                </figure>
+            </li>
+            <li>
+                <figure class="meal-photo">
+                    <img src="resources/img/H.jpg" alt="food-image">
+                </figure>
+            </li>
+
+        </ul>
+
+    </section>
+    <section class="section-steps" id="hiw" data-aos="fade-in" data-aos-duration="2000">
+        <div class="row">
+            <h2>How to works &mdash; Simple as 1,2,3</h2> 
+        </div>
+            <div class="row">
+                <div class="column">
+                    <img data-aos="slide-left" data-aos-duration="2000" src="resources/img/phone.png" alt="phone-image" class="app-screen">
+                </div>
+                <div class="column steps">
+                    <ol class="design">
+                        
+                        
+                        <li>Choose the subscription plan that best fits your needs and sign up today.</li>
+                       
+                       <li>Order your delicious meal using our mobile app or website. Or you can even call us!</li>
+                       
+                        <li>Enjoy your meal after less than 20 minutes. See you the next time!</li>
+                    </ol>
+                </div>
+                <a href="https://www.google.com/" ><img src="resources/img/app store.png" alt="app-store" class="btn-app"></a>
+                <a href="#" ><img src="resources/img/play store.png" alt="play-store" class="btn-app" ></a> 
+                </div>
+               
+                   
+              </div>
+             
+            </div>
+
+                   
+                  
+        </div>
+        
+      </div>
+       <section class="customer-test"   data-aos="fade-in" data-aos-duration="2000">
+         <div class="row2">
+                      <h2>Our Customers can't live without us!</h2>
+        </div>
+        <div class="row">
+            <div class="column2" >
+                <blockquote>
+                    Alpha foodz is just awesome! I just launched a startup which leaves me with no time for cooking, so Alpha foodz is a life-saver. Now that I got used to it, I couldn't live without my daily meals!
+                    <cite><img src="resources/img/customer-1.png" alt="customer-1">Danial Lopez</cite>
+                </blockquote>
+
+            </div>
+
+            <div class="column2">
+                <blockquote>
+                    I was looking for a quick and easy food delivery service. I tried a lot of them and ended up with Alpha foodz. Best food delivery service in the area. Keep up the great work!
+                    <cite><img src="resources/img/customer-1.jpg" alt="customer-2">Pradeep Lamba</cite>
+                </blockquote>
+
+            </div>
+
+            <div class="column2">
+                <blockquote>
+                    Inexpensive, healthy and great-tasting meals, delivered right to my home. We have lots of food delivery here in Lisbon, but no one comes even close to shah'sfood. Me and my family are so in love!
+                    <cite><img src="resources/img/customer-3.jpg" alt="customer-3">Sneha Balwada</cite>
+                </blockquote>
+
+            </div>
+
+        </div>
+        
+        
+       </section>
+
+       <section class="section-plans" id="plans"    data-aos="fade-in" data-aos-duration="2000">
+          <div class="row">
+              <h2>START EATING HEALTHY TODAY</h2>
+
+          </div>
+          <div class="row">
+              <div class="column3">
+                  <div class="plan-box">
+                      <div data-aos="zoom-in" data-aos-duration="2000">
+                          <h3>Premium</h3>
+                          <p class="plan-price"><i class="fal fa-rupee-sign"></i> 25000 <span>/ month</span> </p>
+                          <p class="plan-price-meals">That's only Rs.1028/-  per meal</p>
+                      </div>
+                      <div>
+                          <ul>
+                              <li><ion-icon class="icon-check" name="checkmark"></ion-icon>1 meal every day</li>
+                              <li><ion-icon class="icon-check" name="checkmark"></ion-icon> Order 24/7</li>
+                              <li><ion-icon class="icon-check" name="checkmark"></ion-icon>Access to newest creations</li>
+                              <li><ion-icon class="icon-check" name="checkmark"></ion-icon>Free delivery</li>
+                          </ul>
+                      </div>
+                      <div>
+                          <a href="https://rzp.io/l/HzpWhCWSxb" class="btn btn-full">Pay-now</a>
+                      </div>
+
+                  </div>
+
+              </div>
+
+              <div class="column3">
+                <div class="plan-box">
+                    <div  data-aos="zoom-in" data-aos-duration="2000">
+                        <h3>Pro</h3>
+                        <p class="plan-price"><i class="fal fa-rupee-sign"></i> 12000 <span>/ month</span></p>
+                        <p class="plan-price-meals">That's only Rs.1150/-  per meal</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><ion-icon class="icon-check" name="checkmark"></ion-icon>1 meal every days/month</li>
+                            <li><ion-icon class="icon-check" name="checkmark"></ion-icon>Order 24/7</li>
+                            <li><ion-icon class="icon-check" name="checkmark"></ion-icon>Access to newest creations</li>
+                            <li><ion-icon class="icon-check" name="checkmark"></ion-icon>Free delivery</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <a href="https://rzp.io/l/HzpWhCWSxb" class="btn btn-ghost">Pay-now</a>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="column3">
+                <div class="plan-box">
+                    <div  data-aos="zoom-in" data-aos-duration="2000">
+                        <h3>Starter</h3>
+                        <p class="plan-price"><i class="fal fa-rupee-sign"></i> 1500 <span>/ meals</span></p>
+                        <p class="plan-price-meals"> &nbsp;</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><ion-icon class="icon-check" name="checkmark-circle-outline"></ion-icon></ion-icon>1 meal</li>
+                            <li><ion-icon class="icon-check" name="checkmark-circle-outline"></ion-icon>Order from 8 am to 12 pm</li>
+                            <li><ion-icon class="icon-check" name="close"></ion-icon> </li>
+                            <li><ion-icon class="icon-check" name="checkmark-circle-outline"></ion-icon></ion-icon>Free delivery</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <a href="https://rzp.io/l/HzpWhCWSxb" class="btn btn-ghost">Pay-now</a>
+                    </div>
+
+                </div>
+
+            </div>
+
+           
+
+          </div>
+
+
+       </section>
+
+        
+       
+
+
+       <section class="section-form" id="signup"   data-aos="fade-in" data-aos-duration="4000">
+           <div class="row">
+               <h2>We're happy to hear from you!</h2>
+
+           </div>
+           <div class="row">
+               <form action="signup.php" method="POST" class="contact-form">
+
+                
+                    <div>
+                    &nbsp;&nbsp;&nbsp; <input type="submit" value="Sign-up">
+                    </div>
+                </div>
+                
+
+               </form>
+            </section>    
+            
+            <footer>
+
+                <div class="row">
+                    <div class="column">
+
+                          <ul class="footer-nav">
+                              <li><a href="#">About us</a></li>
+                              <li><a href="#">Blog</a></li>
+                              <li><a href="#">Press</a></li>
+                              <li><a href="#">IOS App</a></li>
+                              <li><a href="#">Android App</a></li>
+                          </ul>
+
+
+                    </div>
+                    <div class="column">
+                        <ul class="social-links">
+                            <li><ion-icon class="fb" name="logo-facebook"></ion-icon></li>
+                            <li><ion-icon class="insta" name="logo-instagram"></ion-icon></li>
+                            <li><ion-icon class="twitter" name="logo-twitter"></ion-icon></li>
+                            <li><ion-icon class="google" name="logo-googleplus"></ion-icon></li>
+                        </ul>
+                    </div>
+                  </div>
+                  <div>
+                      <p class="powered"> Powered by Alpha foodz. All right reserved.</p>
+                  </div>
+            </footer>
+
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+            <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+
+            <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+                          <script>
+                                      AOS.init();
+                                    </script>
+
+
+            <script>
+                $(document).ready(function(){
+                  // Add smooth scrolling to all links
+                  $("a").on('click', function(event) {
+                
+                    // Make sure this.hash has a value before overriding default behavior
+                    if (this.hash !== "") {
+                      // Prevent default anchor click behavior
+                      event.preventDefault();
+                
+                      // Store hash
+                      var hash = this.hash;
+                
+                      // Using jQuery's animate() method to add smooth page scroll
+                      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                      $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                      }, 800, function(){
+                
+                        // Add hash (#) to URL when done scrolling (default click behavior)
+                        window.location.hash = hash;
+                      });
+                    } // End if
+                  });
+                });
+                </script>
+
+</body>
+</html>
